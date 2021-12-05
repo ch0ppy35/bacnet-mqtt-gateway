b4593edc864f8c7f871fd728885968cfb526f73b
Mike Miller <chop3593@gmail.com>
2021-12-04 22:00:36 -0700

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
-const { BacnetClient } = require('./bacnet_client');
-const { Server } = require('./server');
-const { logger } = require('./common');
-const { MqttClient } = require('./mqtt_client');
-const config = require('config');
+const { BacnetClient } = require("./bacnet_client");
+const { Server } = require("./server");
+const { logger } = require("./common");
+// const { MqttClient } = require('./mqtt_client');
+const config = require("config");
 
 // load configs
-const httpServerEnabled = config.get('httpServer.enabled');
+const httpServerEnabled = config.get("httpServer.enabled");
 
 // init MQTT and BACnet clients
 // const mqttClient = new MqttClient();
 const bacnetClient = new BacnetClient();
 
 // called when device has been found
-bacnetClient.on('deviceFound', (device) => {
-    logger.log('info', `Device found: ${device.address}`);
-   // mqttClient.publishMessage(device);
+bacnetClient.on("deviceFound", (device) => {
+  logger.log("info", `Device found: ${device.address}`);
+  // mqttClient.publishMessage(device);
 });
 
 // called when values are polled
-bacnetClient.on('values', (device, values) => {
-    //logger.log('info', `Sending actuals for device ${device.address} to IoT Hub`);
-    logger.log('info', values)
-    //mqttClient.publishMessage(values);
+bacnetClient.on("values", (device, values) => {
+  // logger.log('info', `Sending actuals for device ${device.address} to IoT Hub`);
+  logger.log("info", values);
+  // mqttClient.publishMessage(values);
 });
 
 if (httpServerEnabled) {
-    new Server(bacnetClient);
+  new Server(bacnetClient);
 }
 
 // some default init logic when starting the gateway
-function init() {
-
-}
+function init() {}
 init();
